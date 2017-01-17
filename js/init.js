35272ef87accf96d2f2ece42399e7f3426e43771
James Christopher <jcahall@washington.edu>
2017-01-17 14:48:45 -0800

    content filled out

--- js/init.js
+++ js/init.js
@@ -1,8 +1,3 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
+$(document).ready(function () {
     $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+});
